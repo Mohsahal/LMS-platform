04314a13317ec6e2d8a546f80b6bcbe6f15ba1f8
Mohsahal <mohammedsahal1243@gmail.com>
2025-09-27 23:01:33 +0530

    refresh problem solved

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,19 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
     },
   },
   server: {
+    historyApiFallback: true,
+  },
+  preview: {
     historyApiFallback: true,
   },
 });
